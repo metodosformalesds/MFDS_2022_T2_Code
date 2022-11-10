ee4489e6d89d4f079fc6f7fd23a8be0116887e9a
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-10 02:11:29 -0600

    E-commerce

--- frontend/src/components/product.js
+++ frontend/src/components/product.js
@@ -5,31 +5,32 @@ import { Card } from 'react-bootstrap'
 function Product({product}){
       return(
          <div>
-            <Card className="my-3 p-3 rounded " color={'#264CEB'} bg="dark">
-            <a href={'/product/${product._id}'}>
+            <Card className="my-1 p-1 rounded " color={'#264CEB'} bg="dark">
+            <a href={`/product/${product._id}`}>
                 <Card.Img src={product.image}/>
             </a>
             <Card.Body>
-                <a href to={`/product/${product._id}`}>
+                <a href={`/product/${product._id}`}>
                     <Card.Title as="div">
-                        <strong>{product.name}</strong>
+                        <strong></strong>
                     </Card.Title>
                 </a>
 
+                <Card.Text as="h3">
+                    ${product.price}
+                </Card.Text>
+                 <Card.Text as ="h2">
+                stock: {product.stock}
+                </Card.Text>
                 <Card.Text as="div">
                     <div className="my-3">
                         { product.rating} from {product.numReviews} reviews
                         
                     </div>
                 </Card.Text>
-                <Card.Text as ="h2">
-                stock: {product.stock}
-                </Card.Text>
+               
 
 
-                <Card.Text as="h3">
-                    ${product.price}
-                </Card.Text>
             </Card.Body>
             </Card>
 
