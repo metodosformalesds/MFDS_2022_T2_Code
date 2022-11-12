83e4c1bd4719878711e4b7cfc976dbb8340276a0
VianeyMartinez201145 <89218210+VianeyMartinez201145@users.noreply.github.com>
2022-11-12 16:20:32 -0600

    navbar

--- navbar/App.js
+++ navbar/App.js
@@ -6,26 +6,26 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 export default function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg fixed-top  background-color: #264ECA">
+      <nav className="navbar navbar-expand-lg fixed-top">
         <div className="container"> 
-          <a className="navbar-brand activate Tcolor activate Tsize" href="/" >VetPet</a>
+          <a className="navbar-brand" href="" ><img src="src/logovetpet.jpg"/></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-rg-0">
               <li className="nav-item">
-                <a className="nav-link activate Tcolor" aria-current="page" href="/">Veterinarias</a>
+                <a className="nav-link activate Tcolor " aria-current="page" href="/">Veterinarias</a>
               </li>
               <li className="nav-item ">
-                <a className="nav-link activate Tcolor" aria-current="page" href="/">E-Commerce</a>
+                <a className="nav-link activate Tcolor li.active color" aria-current="page" href="/">E-Commerce</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link activate Tcolor" aria-current="page" href="/">Adopción</a>
+                <a className="nav-link activate Tcolor navbar-nav" aria-current="page" href="/">Adopción</a>
               </li>
             </ul>
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" color='#FFC107'/>
+                <input class="form-control me-2 activate btn-outline-warning" type="search" placeholder="Categoria" aria-label="Search" color='#a10e0e'/>
                 <button type="button" class="btn btn-outline-warning">Buscar</button>
             </form>
           </div>
