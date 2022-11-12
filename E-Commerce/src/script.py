2b38d20bb3d0647a230282be25b397143150706f
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-12 12:02:48 -0600

    E-Commerce

--- E-Commerce/src/script.py
+++ E-Commerce/src/script.py
@@ -8,35 +8,35 @@ headers = {
 'Accept-Language': 'en-US, en;q=0.5'
 }
 
-search_query = 'zep'.replace(' ', '+')
-base_url = 'https://www.amazon.com/s?k={mascotas}'.format(search_query)
+search_query = 'Pet'.replace(' ', '+')
+base_url = 'https://www.amazon.com/s?k={0}'.format(search_query)
 
 items = []
 for i in range(1, 5):
-print('Processing {0}...'.format(base_url + '&page={0}'.format(i)))
-response = requests.get(base_url + '&page={0}'.format(i), headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
+    print('Processing {0}...'.format(base_url + '&page={0}'.format(i)))
+    response = requests.get(base_url + '&page={0}'.format(i), headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
+    results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
 
 for result in results:
-product_name = result.h2.text
+    product_name = result.h2.text
 
 try:
-rating = result.find('i', {'class': 'a-icon'}).text
-rating_count = result.find_all('span', {'aria-label': True})[1].text
+    rating = result.find('i', {'class': 'a-icon'}).text
+    rating_count = result.find_all('span', {'aria-label': True})[1].text
 except AttributeError:
-continue
+    continue
 
 try:
-price1 = result.find('span', {'class': 'a-price-whole'}).text
-price2 = result.find('span', {'class': 'a-price-fraction'}).text
-price = float(price1 + price2)
-product_url = 'https://amazon.com' + result.h2.a['href']
+    price1 = result.find('span', {'class': 'a-price-whole'}).text
+    price2 = result.find('span', {'class': 'a-price-fraction'}).text
+    price = float(price1 + price2)
+    product_url = 'https://amazon.com' + result.h2.a['href']
 # print(rating_count, product_url)
-items.append([product_name, rating, rating_count, price, product_url])
+    items.append([product_name, rating, rating_count, price, product_url])
 except AttributeError:
-continue
+    continue
 sleep(1.5)
 
 df = pd.DataFrame(items, columns=['product', 'rating', 'rating count', 'price', 'product url'])
