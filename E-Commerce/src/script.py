5126e788807da00d9c93b9f1c34b782ffe83654e
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-12 23:29:05 -0600

    E-Commerce

--- E-Commerce/src/script.py
+++ E-Commerce/src/script.py
@@ -8,7 +8,7 @@ headers = {
 'Accept-Language': 'en-US, en;q=0.5'
 }
 
-search_query = 'Pet'.replace(' ', '+')
+search_query = 'Pet products'.replace(' ', '+')
 base_url = 'https://www.amazon.com/s?k={0}'.format(search_query)
 
 items = []
@@ -21,23 +21,25 @@ for i in range(1, 5):
 
 for result in results:
     product_name = result.h2.text
-
+    
     try:
         rating = result.find('i', {'class': 'a-icon'}).text
-        rating_count = result.find_all('span', {'aria-label': True})[1].text
+        
     except AttributeError:
         continue
 
     try:
+        description = result.find('span', {'class': 'a-list-item'}).text
         price1 = result.find('span', {'class': 'a-price-whole'}).text
         price2 = result.find('span', {'class': 'a-price-fraction'}).text
         price = float(price1 + price2)
         product_url = 'https://amazon.com' + result.h2.a['href']
     # print(rating_count, product_url)
-        items.append([product_name, rating, rating_count, price, product_url])
+        print(description)
+        items.append([product_name, rating, description, price, product_url])
     except AttributeError:
         continue
     sleep(1.5)
 
-df = pd.DataFrame(items, columns=['product', 'rating', 'rating count', 'price', 'product url'])
+df = pd.DataFrame(items, columns=['product', 'rating', 'description', 'price', 'product url'])
 df.to_csv('{0}.csv'.format(search_query), index=False)
