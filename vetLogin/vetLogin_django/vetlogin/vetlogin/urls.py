334f699b284e02a75e43c73f9064c4bfe052e157
VianeyMartinez201145 <89218210+VianeyMartinez201145@users.noreply.github.com>
2022-11-13 23:48:16 -0600

    djnago

--- vetLogin/vetLogin_django/vetlogin/vetlogin/urls.py
+++ vetLogin/vetLogin_django/vetlogin/vetlogin/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from users.api.router import router_user
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -35,4 +37,5 @@ urlpatterns = [
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
          cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router_user.urls)),
 ]
