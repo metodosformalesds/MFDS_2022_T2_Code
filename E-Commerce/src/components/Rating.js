b6141411619ac6395d226997fc7d0cc6ec359139
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-15 09:47:42 -0600

    E-Commerce

---
+++ E-Commerce/src/components/Rating.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+
+function Rating ({value, text, color}){
+    return(
+        <div className="rating">
+            <span><i style ={{color}} className={
+                    value >= 1
+                     ? `fas fa-star-half-alt`
+                     : `far fa-star`          
+                }>
+                </i>
+                </span>
+
+                <span><i style ={{color}} className={
+                    value >= 1
+                     ? `fas fa-star-half-alt`
+                     : `far fa-star`          
+                }>
+                </i>
+                </span>
+
+                <span><i style ={{color}} className={
+                    value >= 1
+                     ? `fas fa-star-half-alt`
+                     : `far fa-star`          
+                }>
+                </i>
+                </span>
+
+                <span><i style ={{color}} className={
+                    value >= 1
+                     ? `fas fa-star-half-alt`
+                     : `far fa-star`          
+                }>
+                </i>
+                </span>
+
+                <span><i style ={{color}} className={
+                    value >= 1
+                     ? `fas fa-star-half-alt`
+                     : `far fa-star`          
+                }>
+                </i>
+                </span>
+                
+        </div>
+
+    )
+}
+export default Rating
