e0b709de09c8228d47cb4fd5db9894e106bdbaf1
Daniel Barajas <al193047@alumnos.uacj.mx>
2022-11-17 14:24:30 -0600

    Update settings.py

--- Appointment/project/settings.py
+++ Appointment/project/settings.py
@@ -130,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #Sending E-mails
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'stmp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
