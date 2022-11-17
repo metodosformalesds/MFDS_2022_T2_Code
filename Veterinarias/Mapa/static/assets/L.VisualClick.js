6e7d37c7928b462f865520df6bbe8c5ac5326fbf
Luis Adrian Robles <al180312@alumnos.uacj.mx>
2022-11-17 00:26:44 -0700

    Update map

--- Veterinarias/Mapa/static/assets/L.VisualClick.js
+++ Veterinarias/Mapa/static/assets/L.VisualClick.js
@@ -1,10 +1,3 @@
-/*
-* L.VisualClick
-* Description: A plugin that adds visual feedback when user clicks/taps the map. Useful for when you have a delay on the clickEvents for async fetching of data, or implmentation of Leaflet.singleclick
-* Example: L.visualClick({map: leafletMap}); //Just works
-* Author: Dag Jomar Mersland (twitter: @dagjomar)
-*/
-
 
 L.Map.VisualClick = L.Handler.extend({
 
