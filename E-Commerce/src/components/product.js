e06f6379078be4b800d95fe4751772e41a002a4e
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-17 15:51:02 -0600

    E-Commerce product views

--- E-Commerce/src/components/product.js
+++ E-Commerce/src/components/product.js
@@ -1,25 +1,30 @@
 import React from 'react'
-import { Card } from 'react-bootstrap' //Se manda a llamar al prototipo Card de bootstrap
+import { Card, Button } from 'react-bootstrap' //Se manda a llamar al prototipo Card de bootstrap
 import {Link} from 'react-router-dom';
 import Rating from './Rating'
+import '../App.css';
+
 
 function Product({product}){ //definición de la función producto, que recibe los atributos de la tabla products de la base de datos 8conexión no hecha)
       return(
          <div class="col-sm-8 p-5" >
             <Card className="my-1 p-1 rounded bg-warnin"  >
-            <h4 class="card-title">{product.name}</h4>
-            <Link to={`/product/${product._id}`}>
+            <h4 class="card-title">{product.name_product}</h4>
+            <Link to={`/product/${product.product_id}`}>
                 <Card.Img src={product.image} width="10" alt="Card image cap"/>
             </Link>
             <Card.Body as ="bodyy">
-                <Link href={`/product/${product._id}`}>
+                <Link href={`/product/${product.product_id}`}>
                     <Card.Title as="div">
                         <strong></strong>
                     </Card.Title>
                 </Link>
-
+                
                 <Card.Text as="h3">
-                    ${product.price}
+                    Categoría: {product.category_product_name}
+                </Card.Text>
+                <Card.Text as="h3">
+                    Precio: ${product.price_product}
                 </Card.Text>
                  <Card.Text as ="h2">
                 stock: {product.stock}
@@ -27,12 +32,11 @@ function Product({product}){ //definición de la función producto, que recibe l
                 <Card.Text as="div">
                     <div className="my-3">
                         
-                        <Rating value={product.rating} text ={`${product.numReviews} reviews`} color={'#f8e825'} />                                    
+                        <Rating value={product.rating_product} text ={`${product.numReviews} reviews`} color={'#f8e825'} />                                    
                     </div>
                 </Card.Text>
-               <Card.Text as ="h3">
-                    ${product.price}
-               </Card.Text>
+                <div className="align-center my-"><Button className='btn-block bg-warning' disable={product.countInStock==0} type='button'>Add to cart</Button>
+                <Link href={`/product/${product.product_id}`}>Ver detalles</Link></div>
 
 
             </Card.Body>
