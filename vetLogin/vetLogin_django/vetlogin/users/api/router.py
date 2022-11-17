d9689e6ce58e63a8e901c1be7834b9ff393f1991
VianeyMartinez201145 <89218210+VianeyMartinez201145@users.noreply.github.com>
2022-11-17 14:14:03 -0600

    r

--- vetLogin/vetLogin_django/vetlogin/users/api/router.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from users.api.views import UserApiViewSet
-
-router_user = DefaultRouter()
-
-router_user.register(
-    prefix='users', basename='users', viewset=UserApiViewSet
-)
