df921103c48b0a27ff8623c986aeef24bdba9ee0
paulhernandez200983 <al200983@alumnos.uacj.mx>
2022-11-23 21:06:38 -0600

    E-Commerce final

---
+++ VETPET-Ecommerce/frontend/src/screens/LoginScreen.js
@@ -0,0 +1,98 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import FormContainer from '../components/FormContainer'
+import { login } from '../actions/userActions'
+import Logins from '../components/Logins';
+import { GoogleLogin } from 'react-google-login';
+import GoogleButton from 'react-google-button'
+function LoginScreen({ location, history }) {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const dispatch = useDispatch()
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { error, loading, userInfo } = userLogin
+    const responseGoogle = (response) => {
+        console.log(response);
+      }
+      <GoogleLogin
+      clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={responseGoogle}
+      onFailure={responseGoogle}
+      cookiePolicy={'single_host_origin'}
+    />
+        
+    useEffect(() => {
+        if (userInfo) {
+            history.push(redirect)
+        }
+    }, [history, userInfo, redirect])
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+    }
+
+    return (
+        <FormContainer>
+            <h1>Acceder a cuenta</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader />}
+            <Form onSubmit={submitHandler}>
+
+                <Form.Group controlId='email'>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type='email'
+                        placeholder='Ingresa tu Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+
+                <Form.Group controlId='password'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Ingresa tu Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant='primary'>
+                    Iniciar sesión
+
+                </Button>
+                
+
+            </Form>
+            <GoogleButton
+  onClick={() => { console.log('Google button clicked') }}
+/>
+            
+            <Row className='py-3'>
+                <Col>
+                    Eres cliente nuevo?, si es así solo registrate y listo <Link
+                        to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+                        Registrar
+                        </Link>
+                </Col>
+            </Row>
+
+        </FormContainer>
+    )
+}
+
+export default LoginScreen
