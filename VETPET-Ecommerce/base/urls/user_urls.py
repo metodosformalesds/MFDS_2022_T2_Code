a7fe3956ed5e7f60abc8198051fea94235ad4715
paulhernandez200983 <89218294+paulhernandez200983@users.noreply.github.com>
2022-11-24 12:37:37 -0600

    Update user_urls.py

--- VETPET-Ecommerce/base/urls/user_urls.py
+++ VETPET-Ecommerce/base/urls/user_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from base.views import user_views as views
 
-
+#Se definenlas ruta que van a derivarse del modelo de usuarios, permitiendo su registro, su listado completo, además de los permisos que tienen los miembros de staff, los cuales 
+#permiten ver los perfiles de los usuarios, modificarlos y eliminarlos
 urlpatterns = [
     path('login/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
